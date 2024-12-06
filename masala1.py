39a906c755d5c5de5e0c75ae9e97cd2e3985a610
AkobirRustamov <rustamovakobir8434@gmail.com>
2024-12-06 15:05:45 +0500

    salom111qq

--- masala1.py
+++ masala1.py
@@ -1,3 +1,14 @@
-print("asdfasfdasdfasdf")
-print("asdfasfdasdfasdf")
-print("asdfasfdasdfasdf")
+def solve():
+    import sys
+    input = sys.stdin.read
+    n = input().strip()  # N ni string ko'rinishida o'qish
+    result = []
+
+    for char in n:
+        if char == '0':
+            result.append('1')  # 0 ni 1 bilan almashtirish
+        else:
+            result.append(char)  # Boshqa raqamlarni o'zicha qoldirish
+
+    # Hosil qilingan natijani qaytarish
+    print(''.join(result))
