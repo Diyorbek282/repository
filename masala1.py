376c3af096110993a0ae39c95d4e494da67fbdbd
AkobirRustamov <rustamovakobir8434@gmail.com>
2024-12-06 15:21:03 +0500

    salom111qqdd

--- masala1.py
+++ masala1.py
@@ -1,14 +1,13 @@
-def solve():
-    import sys
-    input = sys.stdin.read
-    n = input().strip()  # N ni string ko'rinishida o'qish
-    result = []
+departure = input().strip()
+arrival = input().strip()
+time_diff = int(input().strip())
 
-    for char in n:
-        if char == '0':
-            result.append('1')  # 0 ni 1 bilan almashtirish
-        else:
-            result.append(char)  # Boshqa raqamlarni o'zicha qoldirish
+s1, m1 = map(int, departure.split(":"))
+s2, m2 = map(int, arrival.split(":"))
+delta=abs(s2-s1)*60+m2-m1
 
-    # Hosil qilingan natijani qaytarish
-    print(''.join(result))
+
+hh = delta // 60
+mm = delta % 60
+
+print(f"{hh}:{mm}")
---
+++ parvoz.py
@@ -0,0 +1,21 @@
+departure = input().strip()
+arrival = input().strip()
+time_diff = int(input().strip())
+
+s1, m1 = map(int, departure.split(":"))
+s2, m2 = map(int, arrival.split(":"))
+
+start_minutes = s1 * 60 + m1
+end_minutes = s2 * 60 + m2
+
+end_minutes += time_diff * 60
+
+if end_minutes < start_minutes:
+    end_minutes += 1440
+
+delta = end_minutes - start_minutes
+
+hh = delta // 60
+mm = delta % 60
+
+print(f"{hh:02}:{mm:02}")
